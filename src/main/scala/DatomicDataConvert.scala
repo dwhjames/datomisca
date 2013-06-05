f4d50deb7f1abd84eb388ab7a6aea2b28f61481a
Pascal Voitot <pascal.voitot.dev@gmail.com>
2013-06-05 11:55:18 +0200

    reprivatize FromDatomicInj to previous bad commit

--- src/main/scala/DatomicDataConvert.scala
+++ src/main/scala/DatomicDataConvert.scala
@@ -19,11 +19,11 @@ package datomisca
   * - 1 DD => 1 Scala type
   * - used when precise type inference by compiler
   */
-trait FromDatomicInj[DD <: DatomicData, A] {
+private[datomisca] trait FromDatomicInj[DD <: DatomicData, A] {
   def from(dd: DD): A
 }
 
-object FromDatomicInj extends FromDatomicInjImplicits {
+private[datomisca] object FromDatomicInj extends FromDatomicInjImplicits {
   def apply[DD <: DatomicData, A](f: DD => A) = new FromDatomicInj[DD, A]{
     def from(dd: DD): A = f(dd)
   }
