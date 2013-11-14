fc92ed4d551614260431602cfbf13df674a3e0d0
Daniel James <daniel.james@pellucid.com>
2013-11-14 12:23:37 -0500

    bump version to 0.7-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ import sbtunidoc.Plugin._
 object DatomiscaBuild extends Build {
 
   lazy val buildSettings = Defaults.defaultSettings ++ Seq(
-      version       := "0.6-SNAPSHOT",
+      version       := "0.7-SNAPSHOT",
       organization  := "com.pellucid",
       scalaVersion  := "2.10.2",
       scalacOptions ++= Seq(
